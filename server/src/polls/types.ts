7a988232a63e264ca0befa0b220b559c1396ae93
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-07-25 14:02:47 +0545

    feat: create a repository type

--- server/src/polls/types.ts
+++ server/src/polls/types.ts
@@ -1,3 +1,5 @@
+// Service Types
+
 export type CreatePollFields = {
   topic: string;
   votesPerVoter: number;
@@ -13,4 +15,18 @@ export type RejoinPollFields = {
   pollID: string;
   name: string;
   userID: string;
+};
+
+// Repository Type
+export type CreatePollData = {
+  pollID: string;
+  topic: string;
+  votesPerVoter: number;
+  userID: string;
+};
+
+export type AddParticipantData = {
+  pollID: string;
+  userID: string;
+  name: string;
 };
