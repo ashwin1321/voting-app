343508622d2182d8864a356582cff5ac0fcb1e84
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-07-25 14:03:24 +0545

    feat: create poll repository to connect to redis

--- server/src/polls/polls.repository.ts
+++ server/src/polls/polls.repository.ts
@@ -1,14 +1,15 @@
-import { Inject } from '@nestjs/common';
+import { Inject, InternalServerErrorException } from '@nestjs/common';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Redis } from 'ioredis';
 import { IORedisKey } from 'src/redis.module';
+import { AddParticipantData, CreatePollData } from './types';
+import { Poll } from 'shared';
 
 @Injectable()
 export class PollsRepository {
   // to use time-to-live from configuration
-
-  private readonly ttl: string; // ttl == time to live
+  private readonly ttl: string;
   private readonly logger = new Logger(PollsRepository.name);
 
   constructor(
@@ -16,5 +17,111 @@ export class PollsRepository {
     @Inject(IORedisKey) private readonly redisClient: Redis,
   ) {
     this.ttl = configService.get('POLL_DURATION');
+  }
+
+  async createPoll({
+    votesPerVoter,
+    topic,
+    pollID,
+    userID,
+  }: CreatePollData): Promise<Poll> {
+    const initialPoll = {
+      id: pollID,
+      topic,
+      votesPerVoter,
+      participants: {},
+      adminID: userID,
+    };
+
+    this.logger.log(
+      `Creating new poll: ${JSON.stringify(initialPoll, null, 2)} with TTL ${
+        this.ttl
+      }`,
+    );
+
+    const key = `polls:${pollID}`;
+
+    try {
+      await this.redisClient
+        .multi([
+          // multi() is a transaction in Redis that will execute all commands in the array atomically
+          ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialPoll)], // JSON.SET sets the value of a key to a JSON object
+          ['expire', key, this.ttl], // expire sets a time-to-live for a key
+        ])
+        .exec(); // exec() executes the transaction
+      return initialPoll;
+    } catch (e) {
+      this.logger.error(
+        `Failed to add poll ${JSON.stringify(initialPoll)}\n${e}`,
+      );
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async getPoll(pollID: string): Promise<Poll> {
+    this.logger.log(`Attempting to get poll with: ${pollID}`);
+
+    const key = `polls:${pollID}`;
+
+    try {
+      const currentPoll = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      ); // this is the same as GET polls:${pollID}
+
+      this.logger.verbose(currentPoll); // verbose is like log but only shows up in development mode
+
+      // if (currentPoll?.hasStarted) {
+      //   throw new BadRequestException('The poll has already started');
+      // }
+
+      return JSON.parse(currentPoll);
+    } catch (e) {
+      this.logger.error(`Failed to get pollID ${pollID}`);
+      throw e;
+    }
+  }
+
+  async addParticipant({
+    pollID,
+    userID,
+    name,
+  }: AddParticipantData): Promise<Poll> {
+    this.logger.log(
+      `Attempting to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+    );
+
+    const key = `polls:${pollID}`;
+    const participantPath = `.participants.${userID}`; // this is the path to the participant in the JSON object
+
+    try {
+      await this.redisClient.send_command(
+        'JSON.SET',
+        key,
+        participantPath,
+        JSON.stringify(name),
+      ); // this is the same as SET polls:${pollID} .participants.${userID} ${JSON.stringify(name)}
+
+      const pollJSON = await this.redisClient.send_command(
+        'JSON.GET',
+        key,
+        '.',
+      );
+
+      const poll = JSON.parse(pollJSON) as Poll;
+
+      this.logger.debug(
+        `Current Participants for pollID: ${pollID}:`,
+        poll.participants,
+      ); // debug is like verbose but shows up in production mode too
+
+      return poll;
+    } catch (e) {
+      this.logger.error(
+        `Failed to add a participant with userID/name: ${userID}/${name} to pollID: ${pollID}`,
+      );
+      throw e;
+    }
   }
 }
